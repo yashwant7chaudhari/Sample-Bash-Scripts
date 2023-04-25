72ce7d52638895a5d1d04fd43c386edd0747ab62
Yashwant Chaudhari <128314684+yashwant7chaudhari@users.noreply.github.com>
2023-04-25 22:05:22 +0530

    Update 13_for.sh

--- scripts/13_for.sh
+++ scripts/13_for.sh
@@ -4,9 +4,9 @@ for VAR1 in java .net python ruby php
 do
   echo "Looping....."
   sleep 1
-  echo "###################################################"
+  echo "###############################"
   echo "Value of VAR1 is $VAR1."
-  echo "###################################################"
+  echo "###############################"
   date
   echo
 done
